d56556451f26d8f7f2f15b6e7faf26e9ad5856c4
Omilabu Elizabeth Wuraola <omilabuw@gmail.com>
2025-01-12 13:21:19 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function showNav(){
+    const nav = document.getElementById('navButton');
+    nav.style.display = 'block'
+}
+function hideNav(){
+    const nav = document.getElementById('navButton');
+    nav.style.display = 'none'
+}
